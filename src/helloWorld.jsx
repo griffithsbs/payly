4f0069f75e41522d7bd9a05b52efcdf0791620ad
Ben Griffiths <benstephengriffiths@gmail.com>
2017-01-23 16:47:36 +0000

    adding components and lib subdirectories within src directory

--- src/helloWorld.jsx
+++ src/helloWorld.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Person from './people/Person.jsx';
-import StaffList from './people/staff/StaffList.jsx';
+import Person from './components/people/Person.jsx';
+import StaffList from './components/people/staff/StaffList.jsx';
 
 import message from './helloWorld2.js';
 
