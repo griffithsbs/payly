fddbd77a04987c7b865271fe5bfe29df91b1b8e9
Ben Griffiths <benstephengriffiths@gmail.com>
2017-01-25 23:27:19 +0000

    adding redux dependency

--- src/helloWorld.jsx
+++ src/helloWorld.jsx
@@ -6,11 +6,15 @@ import StaffList from './components/people/staff/StaffList.jsx';
 
 import message from './helloWorld2.js';
 
+import { createStore } from 'redux';
+
 const staff = [
   { firstName: 'Bob', lastName: 'Jones', emailAddress: 'bob@bobster.com' },
   { firstName: 'Darren', lastName: 'Alan', emailAddress: 'darren@alan.com' },
   { firstName: 'Cressida', lastName: 'Smith', phoneNumber: '0xxxx 1234 56' }
 ];
+
+console.log(createStore); // TODO
 
 ReactDOM.render(
   <div>{message}</div>,
