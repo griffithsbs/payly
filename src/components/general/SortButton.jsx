436a0b91bbe62bab11f69ad4c1f7bba8fa884c35
Ben Griffiths <benstephengriffiths@gmail.com>
2017-02-07 19:02:56 +0000

    refactoring SortButton component into a pure function
    - also turning keyword-spacing eslint rule off

--- src/components/general/SortButton.jsx
+++ src/components/general/SortButton.jsx
@@ -2,31 +2,18 @@ import React from 'react';
 
 import { ascending, descending } from 'lib/orderings';
 
-class SortButton extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this._getLabelFor = this._getLabelFor.bind(this);
+const getLabelFor = direction => {
+    if(direction === ascending) {
+        return 'a-z';
     }
-
-    render() {
-        return (
-          <button onClick={this.props.onSort}>{this.props.label} ({this._getLabelFor(this.props.direction)})</button>
-        );
+    if(direction === descending) {
+        return 'z-a';
     }
+    throw new Error('unrecognised direction');
+};
 
-    // TODO
-    _getLabelFor(direction) {
-        if(direction === ascending) {
-            return 'a-z';
-        }
-        if(direction === descending) {
-            return 'z-a';
-        }
-        throw new Error('unrecognised direction');
-    }
-
-}
+const SortButton = props =>
+  <button onClick={props.onSort}>{props.label} ({getLabelFor(props.direction)})</button>;
 
 SortButton.propTypes = {
     label: React.PropTypes.string.isRequired,
