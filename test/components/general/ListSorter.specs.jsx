81e64af338059046855ea75edb1a5324e04140a8
Ben Griffiths <benstephengriffiths@gmail.com>
2017-02-08 08:26:55 +0000

    adding a couple more tests of the ListSorter component

--- test/components/general/ListSorter.specs.jsx
+++ test/components/general/ListSorter.specs.jsx
@@ -3,13 +3,13 @@ import React from 'react'; // eslint-disable-line no-unused-vars
 import { describe, it } from 'mocha';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import ListSorter from 'components/general/ListSorter';
 import SortButton from 'components/general/SortButton';
 import RadioButtonGroup from 'components/general/RadioButtonGroup';
-import { ascending, descending } from 'lib/orderings';
+import { ascending, descending, sortBy, reverse } from 'lib/orderings';
 
 chai.use(chaiEnzyme());
 chai.use(sinonChai);
@@ -54,13 +54,46 @@ describe('<ListSorter />', () => {
             testSubject.find(SortButton).first().should.have.prop('onSort', SortButton.prototype._onSort);
         });
 
-        // TODO test the sort handler properly
-
         it('with a sort direction of ascending by default', () => {
             const testSubject = shallow(
               <ListSorter label="whatever" onSort={() => {}} sortingOptions={validSortingOptions} />
             );
             testSubject.find(SortButton).first().should.have.prop('direction', ascending);
+        });
+    });
+
+    describe('when the sort button is clicked', () => {
+        it('calls the parent sort handler with the selected sorting function', () => {
+            const parentSortHandler = sinon.spy();
+            const selectedStrategy = { isSelected: true, sortingFunc: () => {}, key: 1 };
+            const sortingOptions = [{ key: 0 }, selectedStrategy, { key: 2 }];
+
+            const testSubject = mount(
+              <ListSorter label="whatever" onSort={parentSortHandler} sortingOptions={sortingOptions} />
+            );
+            testSubject.find('button').simulate('click');
+
+            parentSortHandler.should.have.been.calledWith(selectedStrategy.sortingFunc);
+        });
+
+        it('reverses the selected sorting function and not those that are not selected', () => {
+            const byFooAscending = sortBy(x => x.foo);
+            const byFooDescending = reverse(byFooAscending);
+            const selectedStrategy = { isSelected: true, sortingFunc: byFooAscending, key: 1 };
+            const sortingOptions = [
+                { sortingFunc: byFooAscending, key: 0 },
+                selectedStrategy,
+                { sortingFunc: byFooDescending, key: 2 }
+            ];
+
+            const testSubject = mount(
+              <ListSorter label="whatever" onSort={() => {}} sortingOptions={sortingOptions} />
+            );
+            testSubject.find('button').simulate('click');
+
+            testSubject.state().sortingOptions[0].sortingFunc.should.equal(byFooAscending);
+            testSubject.state().sortingOptions[1].sortingFunc.should.equal(byFooDescending);
+            testSubject.state().sortingOptions[2].sortingFunc.should.equal(byFooDescending);
         });
     });
 });
